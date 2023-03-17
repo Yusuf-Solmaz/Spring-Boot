7a06e3dd1520435b08da9520c1ff18914e61f30c
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-03-18 00:35:08 +0300

    Injecting Custom Properties

--- src/main/java/com/springboot/myapp/controller/MyRestController.java
+++ src/main/java/com/springboot/myapp/controller/MyRestController.java
@@ -1,14 +1,18 @@
 package com.springboot.myapp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestController {
 
+    @Value("${my.name}")
+    private String name;
+
     @GetMapping("/")
     public String sayHello(){
-        return "Hello!!";
+        return "Hello!! " + name;
     }
 
 }
