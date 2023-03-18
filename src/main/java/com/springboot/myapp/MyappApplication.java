17a769db40168a23b1ce633155a3db223a66708a
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-03-18 15:58:37 +0300

    Component scanning

--- src/main/java/com/springboot/myapp/MyappApplication.java
+++ src/main/java/com/springboot/myapp/MyappApplication.java
@@ -3,7 +3,8 @@ package com.springboot.myapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = {"com.springboot.myapp","com.springboot.util"})
 public class MyappApplication {
 
 	public static void main(String[] args) {
