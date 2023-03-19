ecf0343f7383953e9817529d80741667cde421a3
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-03-19 22:15:41 +0300

    Bean Scope (Prototype)

--- src/main/java/com/springboot/myapp/serviceImp/Computer.java
+++ src/main/java/com/springboot/myapp/serviceImp/Computer.java
@@ -1,9 +1,12 @@
 package com.springboot.myapp.serviceImp;
 
 import com.springboot.myapp.service.DoSomething;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) // Default is "singleton"
 public class Computer implements DoSomething {
 
 
