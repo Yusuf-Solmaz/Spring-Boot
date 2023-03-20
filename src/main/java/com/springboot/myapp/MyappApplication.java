fab607fa26bc3623c69c34fab2419a6acd177a92
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-03-20 22:56:38 +0300

    CommandLineRunner

--- src/main/java/com/springboot/myapp/MyappApplication.java
+++ src/main/java/com/springboot/myapp/MyappApplication.java
@@ -1,7 +1,9 @@
 package com.springboot.myapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(
 		scanBasePackages = {"com.springboot.myapp","com.springboot.util"})
@@ -11,4 +13,10 @@ public class MyappApplication {
 		SpringApplication.run(MyappApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String [] args){
+		return runner -> {
+			System.out.println("Hello!");
+		};
+	}
 }
