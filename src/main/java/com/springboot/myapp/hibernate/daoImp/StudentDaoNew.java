007a78dba8c7616f43b214c740f4ad811871ff55
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-03-22 11:20:53 +0300

    Using Primary Annotation

---
+++ src/main/java/com/springboot/myapp/hibernate/daoImp/StudentDaoNew.java
@@ -0,0 +1,13 @@
+package com.springboot.myapp.hibernate.daoImp;
+
+import com.springboot.myapp.hibernate.dao.StudentDao;
+import com.springboot.myapp.hibernate.entity.Student;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentDaoNew implements StudentDao {
+    @Override
+    public void save(Student student) {
+
+    }
+}
