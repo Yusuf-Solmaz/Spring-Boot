b09f8e61efa693aa8645cb8e9d0ad123fd6f866a
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-03-22 11:19:01 +0300

    Update Entity

--- src/main/java/com/springboot/myapp/hibernate/entity/Student.java
+++ src/main/java/com/springboot/myapp/hibernate/entity/Student.java
@@ -1,9 +1,15 @@
 package com.springboot.myapp.hibernate.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "student")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Student {
 
     @Id
@@ -17,4 +23,17 @@ public class Student {
     @Column(name = "student_last_name")
     private String studentLastName;
 
+    public Student(String studentName, String studentLastName) {
+        this.studentName = studentName;
+        this.studentLastName = studentLastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", studentName='" + studentName + '\'' +
+                ", studentLastName='" + studentLastName + '\'' +
+                '}';
+    }
 }
