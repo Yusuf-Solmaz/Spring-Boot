6a2b078ed433b2d918df5ed51c18c52f94cc283a
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-03-27 15:33:33 +0300

    Global Exception Handling

--- src/main/java/com/springboot/myapp/springRest/controller/CarRestController.java
+++ src/main/java/com/springboot/myapp/springRest/controller/CarRestController.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/car")
+@RequestMapping("/cars")
 public class CarRestController {
     List<Car> carList = new ArrayList<>();
 
@@ -37,21 +37,5 @@ public class CarRestController {
         return carList.get(id-1);
     }
 
-    @ExceptionHandler
-    public ResponseEntity<CarErrorResponse> responseEntity (CarNotFoundException exception){
-        CarErrorResponse errorResponse = new CarErrorResponse();
-        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-        errorResponse.setMessage(exception.getMessage());
-        errorResponse.setTime(System.currentTimeMillis());
-        return new ResponseEntity<>(errorResponse,HttpStatus.NOT_FOUND);
-    }
 
-    @ExceptionHandler
-    public ResponseEntity<CarErrorResponse> responseE (Exception exc){
-        CarErrorResponse errorResponse = new CarErrorResponse();
-        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-        errorResponse.setMessage("Enter a number!");
-        errorResponse.setTime(System.currentTimeMillis());
-        return new ResponseEntity<>(errorResponse,HttpStatus.BAD_REQUEST);
-    }
 }
---
+++ src/main/java/com/springboot/myapp/springRest/exceptionHandler/StudentRestExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.springboot.myapp.springRest.exceptionHandler;
+
+import com.springboot.myapp.springRest.exceptions.CarErrorResponse;
+import com.springboot.myapp.springRest.exceptions.CarNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class StudentRestExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<CarErrorResponse> responseEntity (CarNotFoundException exception){
+        CarErrorResponse errorResponse = new CarErrorResponse();
+        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        errorResponse.setMessage(exception.getMessage());
+        errorResponse.setTime(System.currentTimeMillis());
+        return new ResponseEntity<>(errorResponse,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<CarErrorResponse> responseE (Exception exc){
+        CarErrorResponse errorResponse = new CarErrorResponse();
+        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorResponse.setMessage("Enter a number!");
+        errorResponse.setTime(System.currentTimeMillis());
+        return new ResponseEntity<>(errorResponse,HttpStatus.BAD_REQUEST);
+    }
+}
