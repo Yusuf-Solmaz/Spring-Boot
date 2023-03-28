6faad9c846bf22827463f80efa88ecb47d7fa591
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-03-28 15:11:01 +0300

    Adding JPA Repository

---
+++ src/main/java/com/springboot/myapp/springRest/repo/CarRepo.java
@@ -0,0 +1,17 @@
+package com.springboot.myapp.springRest.repo;
+
+import com.springboot.myapp.springRest.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface CarRepo extends JpaRepository<Car,Integer> {
+    @Transactional
+    @Modifying
+    @Query("update Car c set c.color = ?1, c.brand = ?2")
+    int updateColorAndBrandBy(String color, String brand);
+
+}
