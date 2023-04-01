9187f7a223cc66539f89e5f91038d6f1f8794dfc
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-04-01 14:41:57 +0300

    Spring Rest Config

--- src/main/java/com/springboot/myapp/springRest/repo/TrainerRepo.java
+++ src/main/java/com/springboot/myapp/springRest/repo/TrainerRepo.java
@@ -4,11 +4,13 @@ package com.springboot.myapp.springRest.repo;
 import com.springboot.myapp.springRest.entity.Trainer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RepositoryRestResource(path = "members")
 public interface TrainerRepo extends JpaRepository<Trainer,Integer> {
     List<Trainer> findByName(@Param("name") String name);
 
